a4150e496c9da56448ecf6bb2d60b13c7c7938c6
Jason Wiram <jwiram@gmail.com>
2017-04-21 09:00:40 -0700

    Complete project with tablet layout

--- app/src/main/java/com/jasonwiram/smellslikebakin/GridFragment.java
+++ app/src/main/java/com/jasonwiram/smellslikebakin/GridFragment.java
@@ -3,32 +3,36 @@ package com.jasonwiram.smellslikebakin;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ListFragment extends Fragment {
+public class GridFragment extends Fragment {
 
     public interface OnRecipeSelectedInterface {
-        void OnListRecipeSelected(int index);
+        void OnGridRecipeSelected(int index);
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-//        Log.d(LoggingFragment.TAG, "onCreateView");
-        OnRecipeSelectedInterface listener = (OnRecipeSelectedInterface) getActivity();
+        GridFragment.OnRecipeSelectedInterface listener = (GridFragment.OnRecipeSelectedInterface) getActivity();
         View view = inflater.inflate(R.layout.fragment_recyclerview, container, false);
 
         RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
-        ListAdapter listAdapter = new ListAdapter(listener);
-        recyclerView.setAdapter(listAdapter);
-
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        GridAdapter gridAdapter = new GridAdapter(listener);
+        recyclerView.setAdapter(gridAdapter);
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
+        int numColumns = (int) (dpWidth / 200);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity(), numColumns);
         recyclerView.setLayoutManager(layoutManager);
 
         return view;
     }
+
 }
